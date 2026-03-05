99f1cd9a4f8cb91059830f9bf1b0fc6822f95d67
dpluus <shahidkodalida@gmail.com>
2026-03-05 13:54:28 +0530

    Delete play.js

--- play.js
+++
@@ -1,5 +0,0 @@
-window.OBIRAM_PLAYLISTS = [
-    "https://raw.githubusercontent.com/sm-monirulislam/AynaOTT-auto-update-playlist/refs/heads/main/AynaOTT.m3u",
-    "https://raw.githubusercontent.com/biostartvworld/playlist/refs/heads/main/playlist.m3u",
-    "https://raw.githubusercontent.com/sm-monirulislam/RoarZone-Auto-Update-playlist/refs/heads/main/RoarZone.m3u"
-];
