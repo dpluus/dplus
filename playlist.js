d70a74346432886e368bbd934e0c15e2007c419f
dpluus <shahidkodalida@gmail.com>
2026-07-26 20:47:03 +0530

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -11,14 +11,67 @@
       "id": "11",
       "name": "Live 1",
       "type": "iframe",
+      "iframeSrc": "https://asifarena.pages.dev/?id=E1HD"
+    },
+
+{
+      "id": "22",
+      "name": "Live 2",
+      "type": "iframe",
       "iframeSrc": "https://mtv-ply.blogspot.com/p/jw.html?videoUrl=https://bpl24middleeast.akamaized.net/hls/live/2109311/j15klM//master.m3u8"
+    },
+
+{
+      "id": "33",
+      "name": "Live 3",
+      "type": "iframe",
+      "iframeSrc": "https://mtv-ply.blogspot.com/p/jw.html?videoUrl=https://bpl24middleeast.akamaized.net/hls/live/2109311/j15klM//master.m3u8"
+    },
+
+{
+      "id": "ss1",
+      "name": "Star Sports 1",
+      "type": "iframe",
+      "iframeSrc": https://asifarena.pages.dev/?id=E1HD"
+    },
+
+
+{
+      "id": "ss2",
+      "name": "Star Sports 2",
+      "type": "iframe",
+      "iframeSrc": https://asifarena.pages.dev/?id=E2HD"
+    },
+
+
+{
+      "id": "ss3",
+      "name": "Star Sports 3",
+      "type": "iframe",
+      "iframeSrc": https://asifarena.pages.dev/?id=SS3"
+    },
+
+
+{
+      "id": "ssh1",
+      "name": "Star Sports 1 Hindi",
+      "type": "iframe",
+      "iframeSrc": https://asifarena.pages.dev/?id=H1HD"
+    },
+
+
+{
+      "id": "ssh2",
+      "name": "Star Sports 2 Hindi",
+      "type": "iframe",
+      "iframeSrc": https://asifarena.pages.dev/?id=H2HD"
     },
 
 
 
     {
-      "id": "ss3",
-      "name": "Star Sport 3",
+      "id": "ss",
+      "name": "Star Sport ",
       "type": "iframe",
       "iframeSrc": "http://asifarena.pages.dev/?id=SS3"
     }
