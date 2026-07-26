8d496818d6f9b7502e0ae3d597a2dbfb231f2fdc
dpluus <shahidkodalida@gmail.com>
2026-07-26 16:51:17 +0530

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -6,6 +6,16 @@
       "type": "iframe",
       "iframeSrc": "https://cinesecret.pages.dev/goltv"
     },
+
+{
+      "id": "11",
+      "name": "Live 1",
+      "type": "iframe",
+      "iframeSrc": "https://mtv-ply.blogspot.com/p/jw.html?videoUrl=https://bpl24middleeast.akamaized.net/hls/live/2109311/j15klM//master.m3u8"
+    },
+
+
+
     {
       "id": "ss3",
       "name": "Star Sport 3",
